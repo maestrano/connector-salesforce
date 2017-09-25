1a925e4dff84519ed93cf87498efdcd560994e7c
marcocode <admin@hecticmojo.com>
2017-09-25 16:38:01 +0100

    Upgrade framework, add pry-byebug and use evn sidekiq config

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,5 @@ module ConnectorSalesforce
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    # ActiveJob config
-    config.active_job.queue_adapter = :sidekiq
-
-    # Redis caching
-    config.cache_store = :redis_store if ENV['REDIS_URL']
   end
 end
